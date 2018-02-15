62edb1dedcd988fcf9e9b007f073e52655167e8f
Klaus Löffelmann <klaus@loeffelmann.de>
2018-02-15 12:44:10 +0100

    Create demodata for categories.

--- src/RoamingClipBoardApi/ClientApp/app/categories/categories.component.ts
+++ src/RoamingClipBoardApi/ClientApp/app/categories/categories.component.ts
@@ -7,3 +7,32 @@
 export class CategoriesComponent {
     pageTitel: string = 'Clipboard Categories';
 }
+
+export class CategoriesMock {
+    public categories = [{
+        description: "Car related",
+        lastUsed: "2018-02-15",
+        linkCount: 9
+    }, {
+            description: "Azure",
+            lastUsed: "2018-02-15",
+            linkCount: 13
+        }, {
+            description: "Commodore 64",
+            lastUsed: "2018-02-11",
+            linkCount: 7
+        }, {
+            description: "Visual Basic",
+            lastUsed: "2018-02-16",
+            linkCount: 11
+        }, {
+            description: ".NET",
+            lastUsed: "2018-02-16",
+            linkCount: 5
+        }, {
+            description: "C#",
+            lastUsed: "2017-12-24",
+            linkCount: 2
+        }
+    ]
+}
