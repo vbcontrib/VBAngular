fcb8534b06d0956d30b8d8f403ddf4ce8326d9e2
Deborah Kurata <deborahk@insteptech.com>
2018-02-20 13:01:11 -0800

    Add comments; Add sample code for Observables.

--- src/RoamingClipBoardApi/ClientApp/app/categories/categories.component.ts
+++ src/RoamingClipBoardApi/ClientApp/app/categories/categories.component.ts
@@ -1,24 +1,33 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { CategoryDataService } from '../shared/categoryDataService';
+
 import { getCalendarFormat } from 'ngx-bootstrap/chronos/moment/calendar';
+import { Category } from '../../Models/catergory';  // <-- Note that this folder is misspelled
 
 @Component({
     selector: 'roamclip-categories',
     templateUrl: './categories.component.html'
 })
 export class CategoriesComponent implements OnInit {
-    constructor(private data: CategoryDataService) {
-    }
-
+    // Normally the properties are first, before the constructor
+    // No need for "public". Properties are public by default.
     tableHeader: string = 'Clipboard Categories';
-    public categories = [];
+    categories: Category[] = [];
+    errorMessage: string;
 
+    constructor(private data: CategoryDataService) { }
+
+    // I added the code for the Observable. You can comment this out to use the async technique instead.
     ngOnInit(): void {
-        this.getCategories().catch(() => { });
-        };
+        // this.getCategories().catch(() => { });
+        this.data.loadCategories().subscribe(
+            (categories: Category[]) => this.categories = categories,
+            (error: any) => this.errorMessage = <any>error
+        );
+    };
 
     async getCategories() {
-        this.categories = await this.data.loadCategories();
+        this.categories = await this.data.loadCategoriesAsync();
     }
 }
 
--- src/RoamingClipBoardApi/ClientApp/app/links/links.component.ts
+++ src/RoamingClipBoardApi/ClientApp/app/links/links.component.ts
@@ -1,37 +1,40 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { LinkDataService } from '../shared/linkDataService';
 
+import { Link } from '../../Models/link';
+
 @Component({
     selector: 'roamclip-links',
     templateUrl: './links.component.html'
 })
 export class LinksComponent implements OnInit {
+    tableHeader: string = 'Clipboard Links';
+    links: Link[];
+
     constructor(private data: LinkDataService) {
     }
-
-    tableHeader: string = 'Clipboard Links';
-    public links = [];
 
     ngOnInit(): void {
         this.data.loadLinks().
             subscribe((result) => {
 
                 if (result) {
-                    this.links = this.data.links;
+                    this.links = result;
                 }
                 else {
 
-                    this.links = [{
-                        link: "https://suchen.mobile.de/fahrzeuge/search.html?damageUnrepaired=NO_DAMAGE_UNREPAIRED&isSearchRequest=true&makeModelVariant1.makeId=17200&makeModelVariant1.modelDescription=se&makeModelVariant1.modelId=134&maxFirstRegistrationDate=1980-12-31&maxPowerAsArray=PS&minPowerAsArray=PS&scopeId=C&transmissions=AUTOMATIC_GEAR",
-                        description: "Search for classing Mercedes W123 280 SE",
-                        categoryName: "Car related",
-                        bestBefore: "2018-04-30"
-                    }, {
-                        link: "https://suchen.mobile.de/fahrzeuge/details.html?id=252201547&damageUnrepaired=NO_DAMAGE_UNREPAIRED&isSearchRequest=true&makeModelVariant1.makeId=17200&makeModelVariant1.modelDescription=se&makeModelVariant1.modelId=134&maxFirstRegistrationDate=1980-12-31&pageNumber=1&scopeId=C&transmissions=AUTOMATIC_GEAR&action=topOfPage&top=3:3&searchId=94d62f01-b49f-344c-6a46-d8126b301948",
-                        description: "Classic Red Mercedes 280SE",
-                        categoryName: "car related",
-                        bestBefore: "2018-04-30"
-                    }]
+                    //this.links = [{
+                    //    link: "https://suchen.mobile.de/fahrzeuge/search.html?damageUnrepaired=NO_DAMAGE_UNREPAIRED&isSearchRequest=true&makeModelVariant1.makeId=17200&makeModelVariant1.modelDescription=se&makeModelVariant1.modelId=134&maxFirstRegistrationDate=1980-12-31&maxPowerAsArray=PS&minPowerAsArray=PS&scopeId=C&transmissions=AUTOMATIC_GEAR",
+                    //    description: "Search for classing Mercedes W123 280 SE",
+                    //    categoryName: "Car related",
+                    //    bestBefore: "2018-04-30"
+                    //}, 
+                    //{
+                    //    link: "https://suchen.mobile.de/fahrzeuge/details.html?id=252201547&damageUnrepaired=NO_DAMAGE_UNREPAIRED&isSearchRequest=true&makeModelVariant1.makeId=17200&makeModelVariant1.modelDescription=se&makeModelVariant1.modelId=134&maxFirstRegistrationDate=1980-12-31&pageNumber=1&scopeId=C&transmissions=AUTOMATIC_GEAR&action=topOfPage&top=3:3&searchId=94d62f01-b49f-344c-6a46-d8126b301948",
+                    //    description: "Classic Red Mercedes 280SE",
+                    //    categoryName: "car related",
+                    //    bestBefore: "2018-04-30"
+                    //}]
                 }
             })
     }
